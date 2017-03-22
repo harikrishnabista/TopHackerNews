165b0ec70d2718341deedd8cb60b84952ca8294f
Hari Krishna Bista <harikrista@gmail.com>
2017-03-22 11:21:53 -0500

    TopHackerNewsManager add to the plugin to help plugin to make http call and provide response to the cdvplugin

--- src/ios/TopHackerNewsPlugin.m
+++ src/ios/TopHackerNewsPlugin.m
@@ -1,34 +1,33 @@
 #import "TopHackerNewsPlugin.h"
+#import "helper.h"
+#import "TopHackerNewsManager.h"
 
 @implementation TopHackerNewsPlugin
 
 - (void)showHackerNews:(CDVInvokedUrlCommand*)command
 {
-	// TopHackerNewsManager *newManager = [[TopHackerNewsManager alloc] init];
- //    [newManager initSDK];
- //    [newManager showHackerNews:@"" completion:^(NSArray *top20News) {
- //        for (NSString *str in top20News) {
- //            NSLog(@"%@",str);
- //        }
- //    }];
-
- //    NSString* name = [[command arguments] objectAtIndex:0];
- //    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
- //    CDVPluginResult* result = [CDVPluginResult
- //                               resultWithStatus:CDVCommandStatus_OK
- //                               messageAsString:msg];
-
- //    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-    
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-    
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        TopHackerNewsManager *newManager = [[TopHackerNewsManager alloc] init];
+        [newManager initSDK];
+        
+        [newManager showHackerNews:^(NSArray *top20News) {
+            
+            NSString * msg = @"";
+            
+            for (NSString *str in top20News) {
+                NSLog(@"%@",str);
+                msg = [msg stringByAppendingString:str];
+            }
+            
+            NSLog(@"%@",msg);
+            
+            CDVPluginResult* result = [CDVPluginResult
+                                       resultWithStatus:CDVCommandStatus_OK
+                                       messageAsString:msg];
+            
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }];
+    }];
 }
 
 @end
